7b617e016c7fa6c027eb90049f4ffd9220eff1a1
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-11-21 18:54:13 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
-def my_select(collection)
- # your code here!
+def my_select(array) 
+counter = 0
+result = []
+
+while count < array.size
+  if yield(array[counter]) == true
+  result.push(array[counter])
+end
+  count+=1
+end
+result
 end
